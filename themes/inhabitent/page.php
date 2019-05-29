9b5eb5bee916b0e4784997d192d9be7d6fdea292
Vincent Wang <a36168120@gmail.com>
2019-05-29 06:40:24 -0700

    tiddy up Find-Us page

--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -4,19 +4,21 @@
  */
 
 get_header(); ?>
-<section class="page-wrapper">
+<section class="find-us-content">
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-find-us" role="main">
 			
 			<?php while ( have_posts() ) : the_post(); ?>
 			
+			
 			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			
 			
 			<?php endwhile; // End of the loop. ?>
 			
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+	
 	<div class="page-sidebar">
 		<?php get_sidebar(); ?>
 	</div>
