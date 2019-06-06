23129888aa0d23480efdc2107c64e3d2cd39272f
Vincent Wang <a36168120@gmail.com>
2019-06-06 06:56:34 -0700

    fix styling for header and footer, add jQuery for header search bar - have issue on closing

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -36,7 +36,6 @@
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html('Primary Menu'); ?></button>
 						<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu'));?>
 
-
 						<div class="search-form">
 							<?php get_search_form();?>
 						</div>
--- themes/inhabitent/js/script.js
+++ themes/inhabitent/js/script.js
@@ -1,6 +1,24 @@
-// (function ($) {
-   
-//     $('body').append('hello world');
-// })(jQuery);
+(function ($) {
+    $(function() {
+
+        const searchBar = $('.search-field');
+        // const selected = this.value;
+    
+        searchBar.hide();
+        $('.search-submit').on('click', function(event) {
+          event.preventDefault();
+          searchBar.toggle('slide');
+          searchBar.focus();
+        });
+    
+        // searchBar.on('blur', function(event) {
+        //   event.preventDefault();
+        //   if (selected === '') {
+        //       searchBar.hide('slide');
+        //   }
+        // });
+    });
+
+})(jQuery);
 
 
