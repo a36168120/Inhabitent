7a20bc7645246941513f30d366ca4319ba820eb5
Vincent Wang <a36168120@gmail.com>
2019-06-06 05:08:38 -0700

    fix & style up about page

--- themes/inhabitent/page-about.php
+++ themes/inhabitent/page-about.php
@@ -16,32 +16,6 @@ get_header(); ?>
 			
 			<?php endwhile; // End of the loop. ?>
 
-			<!-- <section class="about-sections">
-				<h2>Our Story</h2>
-				<div>
-					<p> 
-						Inhabitent Camping Supply Co. has been Vancouver camping supply icon for more than two whole months! 
-						Customers often comment on the bustle of activity they see in store…that’s where the magic happens every day.
-					</p>
-					<p>
-						We want to bridge the gap between the comfort of city life and the lovely Instagram-worthiness of the great outdoors 
-						that surround us. We sell gear that’s fun and functional. 
-						So much fun, in fact, that you’ll want to pitch a tent inside your one-bedroom apartment so you can use it every day.
-					</p>
-				</div>
-
-				<h2>Our Team</h2>
-				<div>
-					<p>
-						Inhabitent Camping Supply Co.’s staff is made up of an amazing team of inspired retail associates. 
-						We really know our stuff when it comes to travel hammocks and campfire cooking gadgets. 
-						From a provincial park campground to the back-country, our staff knows what you need to outfit your outdoor outing.
-					</p>
-					<p>
-						Our shop is nestled away in a lovely little corner of Vancouver. Pop in, say hi, and try out our tents!
-					</p>
-				</div>
-			</section> -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 </section>
